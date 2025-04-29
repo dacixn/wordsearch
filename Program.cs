93fa03ba28771577cfcc8d376dcf28d03d9ca84d
daci <git@dacian.co>
2025-04-29 12:22:16 -0400

    restructure

--- Program.cs
+++ Program.cs
@@ -1,16 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WordSearch
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("hello");
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
